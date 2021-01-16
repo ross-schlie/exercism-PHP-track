7eb6f1b13a7f6a9a435b0405600a012e5fd68a30
Ross Schlie <ross.c.schlie@gmail.com>
2021-01-15 23:57:28 -0800

    Exercism PHP hello world solution

---
+++ hello-world/HelloWorldTest.php
@@ -0,0 +1,15 @@
+<?php
+
+class HelloWorldTest extends PHPUnit\Framework\TestCase
+{
+    public static function setUpBeforeClass() : void
+    {
+        require_once 'hello-world.php';
+    }
+
+    // @test
+    public function testHelloWorld() : void
+    {
+        $this->assertEquals('Hello, World!', helloWorld());
+    }
+}
